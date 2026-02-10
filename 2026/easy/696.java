4518fd39e143b2ab67e715487e57fa146ead8754
Rustem Stilinski <48765033+Rustem-Stilinski@users.noreply.github.com>
2026-02-10 11:49:04 -0500

    Create 696.java

---
+++ 2026/easy/696.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int prevCount = 0;
+        int currentCount = 1;
+        int result = 0;
+        char[] chars = s.toCharArray();
+
+        for(int i=1;i<chars.length;i++){
+            if(chars[i-1]!=chars[i]){
+                            result = result + Math.min(prevCount, currentCount);
+                prevCount = currentCount;
+                currentCount=1;
+
+            }
+            else{
+                currentCount++;
+            }
+        }
+            result = result+ Math.min(prevCount, currentCount);
+      
+        return result;
+    }
+}
