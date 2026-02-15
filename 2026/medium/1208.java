bf3109d04a9924500ecd60d936aa6667ee65a5fc
Rustem Stilinski <48765033+Rustem-Stilinski@users.noreply.github.com>
2026-02-15 11:18:08 -0500

    Create 1208.java

---
+++ 2026/medium/1208.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int equalSubstring(String s, String t, int maxCost) {
+        int result = 0;
+        int left = 0;
+        int currentCost = 0;
+        int n = s.length();
+        for(int right = 0 ; right<n; right++){
+            currentCost += Math.abs(s.charAt(right) - t.charAt(right));
+
+            while(currentCost>maxCost){
+                currentCost-=Math.abs(s.charAt(left) - t.charAt(left));
+                left++;
+            }
+
+            result = Math.max(result, right-left+1);
+        }
+        
+        return result;
+    }
+}
