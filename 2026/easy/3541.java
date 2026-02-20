da7251826a4e2ece84be8201f16789e461bdb5c1
Rustem Stilinski <48765033+Rustem-Stilinski@users.noreply.github.com>
2026-02-20 13:10:48 -0500

    Create 3541.java
    Beats only 10%

---
+++ 2026/easy/3541.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxFreqSum(String s) {
+        Map<Character, Integer> vowelCharToFreqMap = new HashMap<>();
+        Map<Character, Integer> consonantCharToFreqMap = new HashMap<>();  
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+        int maxVowels = 0;  
+        int consonantMax = 0;  
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (vowels.contains(c)) {
+                int freq = vowelCharToFreqMap.merge(c, 1, Integer::sum);
+                maxVowels = Math.max(maxVowels, freq);  
+            } else {
+                int freq = consonantCharToFreqMap.merge(c, 1, Integer::sum);
+                consonantMax = Math.max(consonantMax, freq);  
+            }
+        }
+        
+        return consonantMax + maxVowels;
+    }
+}
